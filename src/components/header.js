9712d8c3ef21150ee4b50d4a476e868eeeee01fd
Ed Ignot <edignot@gmail.com>
2020-07-25 09:44:47 -0600

    Header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,33 @@
 import { Link } from "gatsby"
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Nav from "./nav"
+import Img from "gatsby-image"
 
-const Header = () => (
-  <header>
-    <div className="nav-bar">
-      <Link to="/">LOGO</Link>
-      <Nav position="top-nav" />
-    </div>
-    <h1>PROJECTS</h1>
-  </header>
-)
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header>
+      <div className="nav-bar">
+        <Link to="/">
+          <Img fluid={data.file.childImageSharp.fluid} className="logo" />
+        </Link>
+        <Nav position="top-nav" />
+      </div>
+      <h1 className="page-title">PROJECTS</h1>
+    </header>
+  )
+}
 
 export default Header
