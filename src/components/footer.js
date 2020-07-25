f4dcc72d199d9f8104eb9e1b4a80ba93c58c2032
Ed Ignot <edignot@gmail.com>
2020-07-25 16:51:26 -0600

    Media link setup

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,36 @@
 import { Link } from "gatsby"
 import React from "react"
 
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  FaTwitterSquare,
+  FaCamera,
+} from "react-icons/fa"
+import { MdMail } from "react-icons/md"
+
 const Footer = ({ siteTitle }) => (
   <footer>
-      <p>
-        <Link to="/">{siteTitle}</Link>
-        © {new Date().getFullYear()}
-      </p>
+    <section className="footer-icons">
+      <a href="https://github.com/edignot">
+        <FaGithubSquare />
+      </a>
+      <a href="https://www.linkedin.com/feed/">
+        <FaLinkedin />
+      </a>
+      <a href="https://twitter.com/EditaIgnot">
+        <FaTwitterSquare />
+      </a>
+      <a href="mailto:edignot@gmail.com">
+        <MdMail />
+      </a>
+      <a href="https://editaignot.com/">
+        <FaCamera />
+      </a>
+    </section>
+    <p>
+      <Link to="/">{siteTitle}</Link>© {new Date().getFullYear()}
+    </p>
   </footer>
 )
 
