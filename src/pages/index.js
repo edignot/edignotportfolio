745e49f013d959ab18c3c3d8210c77ce35d960a7
Ed Ignot <edignot@gmail.com>
2020-07-27 11:11:14 -0600

    Message updte

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,17 @@ import Nav from "../components/nav"
 import ParticleContainer from "../components/particles"
 import "../css/index.css"
 
-const Index = () => (
-  <section className="home">
-    <SEO title="home" />
-    <Nav position="center-nav" />
-    <ParticleContainer size="big" />
-    console.log('I'm looking for my next software developer role! Please reach
-    out edignot@gmail.com')
-  </section>
-)
+const Index = () => {
+  console.log(
+    "I'm looking for my next software developer role! Please reach out edignot@gmail.com"
+  )
+  return (
+    <section className="home">
+      <SEO title="home" />
+      <Nav position="center-nav" />
+      <ParticleContainer size="big" />
+    </section>
+  )
+}
 
 export default Index
